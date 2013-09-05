c388d67a6be9e62158babfd65994dc05d9f13bc4
Guillaume Sorbier <guillaume.sorbier@gmail.com>
2013-09-05 06:39:49 +0200

    Ajout du mode paredit et tentative de faire un hook kill-emacs-hook.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,7 +6,7 @@
 (require 'package)
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (require 'ido)
 
@@ -31,12 +31,12 @@
 (setq inhibit-splash-scr t)
 (setq locate-command "mdfind")
 (setq
-   backup-by-copying t
-   backup-directory-alist '(("." . "~/.saves"))
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)
+ backup-by-copying t
+ backup-directory-alist '(("." . "~/.saves"))
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)
 (setq backup-directory-alist
           `((".*" . ,temporary-file-directory)))
     (setq auto-save-file-name-transforms
@@ -67,8 +67,23 @@
 (load-theme 'wombat t)
 
 (defun switch-to-previous-buffer ()
-      (interactive)
-      (switch-to-buffer (other-buffer (current-buffer) 1)))
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+
+
+;; (add-hook 'kill-emacs-hook
+;; 	  (append-to-file "brume"
+;; 			  nil
+;; 			  ((expand-file-name "dotfiles/emacs.d/packages" (getenv "HOME")))))
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+    (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+    (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+    (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+    (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
 (recentf-mode t)
 (ido-mode t)
--- emacs/init.el
+++
@@ -1,9 +0,0 @@
-(require 'package)
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-(require 'ido)
-
-(ido-mode t)
-(package-initialize)
